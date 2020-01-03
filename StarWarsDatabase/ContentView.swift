e3c6d41fd5faae39aea4c8c63c5fc0aa7edd7a30
Joel Junior <joel.asj2@gmail.com>
2020-01-03 11:01:54 -0300

    Initial Commit

---
+++ StarWarsDatabase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarWarsDatabase
+//
+//  Created by Joel Júnior on 03/01/20.
+//  Copyright © 2020 jnr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWarsDatabaseTests/StarWarsDatabaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  StarWarsDatabaseTests.swift
+//  StarWarsDatabaseTests
+//
+//  Created by Joel Júnior on 03/01/20.
+//  Copyright © 2020 jnr. All rights reserved.
+//
+
+import XCTest
+@testable import StarWarsDatabase
+
+class StarWarsDatabaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
